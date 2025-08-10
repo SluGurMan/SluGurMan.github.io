2f12720e6e17355ab23c0162eb614fd5a569a9c5
SluGurMan <119013429+SluGurMan@users.noreply.github.com>
2025-08-10 06:40:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co'
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY'
+const SUPABASE_URL = 'https://ccpyrrvicpklepidtilo.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjcHlycnZpY3BrbGVwaWR0aWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3OTI5NTcsImV4cCI6MjA3MDM2ODk1N30.BIsUcuDylm0x7DO32YJ87TuVm6oP2zysEQSCjM-X18s'
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 const loginBtn = document.getElementById('login-btn')
